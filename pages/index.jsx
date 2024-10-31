f7430e3d22ea1d10b6519dfb4f4523080c64e061
thiagowilker <43474901+thiagowilker@users.noreply.github.com>
2024-10-31 15:57:37 +0000

    feat: adiciona pagina inicial

---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+import { Html } from "next/document"
+
+function Home() {
+    return <h1>Oiiii Kamylaaaaaaaa minha rainhaaaaaaa, eu te amo muitooooooo &#x1F49A;. Se você me ama da um sorrizinho &#129392;.</h1>
+}
+
+export default Home
