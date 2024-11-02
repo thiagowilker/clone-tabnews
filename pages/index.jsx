437fc0f75af28ec96167acd49dc313991093ea8d
thiagowilker <43474901+thiagowilker@users.noreply.github.com>
2024-11-02 04:20:59 +0000

    refactor: atualiza pagina inicial

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,10 @@
 import { Html } from "next/document"
 
 function Home() {
-    return <h1>Oiiii Kamylaaaaaaaa minha rainhaaaaaaa, eu te amo muitooooooo &#x1F49A;. Se você me ama da um sorrizinho &#129392;.</h1>
+    return (
+        <h1>Oiiii Kamylaaaaaaaa minha rainhaaaaaaa, eu te amooooo &#x1F49A;&#x1F499;.<br />Se você me ama da um sorrizinho &#128591;&#129392;.</h1>
+    )
+
 }
 
 export default Home
