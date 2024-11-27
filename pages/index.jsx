41d68d5bcec2c1d64408c5d86b2dc2d50a0381cd
thiagowilker <43474901+thiagowilker@users.noreply.github.com>
2024-11-27 02:03:03 +0000

    feat: adiciona scripts `lint:check` e `lint:fix`

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,13 @@
-import { Html } from "next/document"
+import { Html } from "next/document";
 
 function Home() {
-    return (
-        <h1>Oiiii Kamylaaaaaaaa minha rainhaaaaaaa, eu te amooooo &#x1F49A;&#x1F499;.<br />Se você me ama da um sorrizinho &#128591;&#129392;.</h1>
-    )
-
+  return (
+    <h1>
+      Oiiii Kamylaaaaaaaa minha rainhaaaaaaa, eu te amooooo &#x1F49A;&#x1F499;.
+      <br />
+      Se você me ama da um sorrizinho &#128591;&#129392;.
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
