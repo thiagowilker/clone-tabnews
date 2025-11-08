4e5df62a3b6a45e689499f08e62e98c892008ac7
thiagowilker <43474901+thiagowilker@users.noreply.github.com>
2025-11-08 23:57:39 +0000

    feat: finaliza o endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,29 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const databaseName = process.env.POSTGRES_DB;
+  const currentISODate = new Date().toISOString();
+  const versionQueryResult = await database.query("SHOW server_version;");
+  const maxConnectionsQueryResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const openConnectionsQueryResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    updated_at: currentISODate,
+    dependencies: {
+      database: {
+        version: versionQueryResult.rows[0].server_version,
+        max_connections: parseInt(
+          maxConnectionsQueryResult.rows[0].max_connections,
+        ),
+        open_connections: openConnectionsQueryResult.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+
   expect(response.status).toBe(200);
+});
+
+test("GET to /api/v1/status should return correctly formatted response body", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(responseBody).toEqual({
+    updated_at: new Date(responseBody.updated_at).toISOString(),
+    dependencies: {
+      database: {
+        version: "16.0",
+        max_connections: 100,
+        open_connections: 1,
+      },
+    },
+  });
 });
