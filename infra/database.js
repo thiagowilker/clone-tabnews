ac912ab34d9a19be008b58506775448648f5cf80
thiagowilker <43474901+thiagowilker@users.noreply.github.com>
2025-11-22 18:11:49 +0000

    feat: implementa endpoint `/api/v1/migrations`

--- infra/database.js
+++ infra/database.js
@@ -1,26 +1,45 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
+  let client;
+
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client?.end();
+  }
+}
+
+async function getNewClient() {
   const client = new Client({
     database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_HOST,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValue(),
   });
+
   await client.connect();
 
-  try {
-    const result = await client.query(queryObject);
-    return result;
-  } catch (err) {
-    console.error(err);
-  } finally {
-    await client.end();
+  return client;
+}
+
+function getSSLValue() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
   }
+
+  return process.env.NODE_ENV === "production" ? true : false;
 }
 
 export default {
   query,
+  getNewClient,
 };
---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,30 @@
+import database from "infra/database";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const firstResponse = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  const firstResponseBody = await firstResponse.json();
+
+  expect(firstResponse.status).toBe(201);
+  expect(Array.isArray(firstResponseBody)).toBe(true);
+  expect(firstResponseBody.length).toBeGreaterThan(0);
+
+  const secondResponse = await fetch(
+    "http://localhost:3000/api/v1/migrations",
+    {
+      method: "POST",
+    },
+  );
+  const secondResponseBody = await secondResponse.json();
+
+  expect(secondResponse.status).toBe(200);
+  expect(Array.isArray(firstResponseBody)).toBe(true);
+  expect(secondResponseBody.length).toBe(0);
+});
